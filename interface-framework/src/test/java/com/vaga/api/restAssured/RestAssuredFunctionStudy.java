5d88dc9959eaece5ee60e3af2b2819bc08d21daa
vagabond1_1983 <vagabond1_1983@163.com>
2020-03-15 15:04:54 +0800

    rest-assured basic study

--- interface-framework/src/test/java/com/vaga/api/restAssured/RestAssuredFunctionStudy.java
+++ interface-framework/src/test/java/com/vaga/api/restAssured/RestAssuredFunctionStudy.java
@@ -1,6 +1,8 @@
 package com.vaga.api.restAssured;
 
 import com.github.fge.jsonschema.main.JsonSchemaFactory;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
@@ -17,18 +19,18 @@ import static io.restassured.module.jsv.JsonSchemaValidator.*;
  * @description rest assured功能探索
  */
 public class RestAssuredFunctionStudy {
+    private RequestSpecification requestSpecification;
+    @BeforeClass
+    public void init() {
+        requestSpecification = given().when().baseUri("http://www.sosoapi.com");
+    }
 
     // 基础get请求
     @Test
     public void getUsersTest() {
-
-        Map<String, String> params = new HashMap<String, String>();
-        params.put("pageNum", "1");
-        params.put("pageSize", "10");
-        given()
+        requestSpecification
                 .when()
-                .baseUri("http://www.sosoapi.com")
-                .get("/demo/swagger/user/simple/list.htm")
+                .get("/demo/swagger/user/simple/list.htm?pageSize={pageSize}&pageNum={pageNum}",10,1)
                 .then()
                 .log().all(true)
                 .body("userId", hasItems(1, 2));
@@ -39,13 +41,28 @@ public class RestAssuredFunctionStudy {
     @Test
     public void jsonSchemaValidateTest() {
         JsonSchemaFactory jsonSchemaFactory = JsonSchemaFactory.byDefault();
-        given()
+        requestSpecification
                 .when()
-                .baseUri("http://www.sosoapi.com")
                 .get("/demo/swagger/user/simple/list.htm")
+
                 .then()
                 .assertThat()
                 .body(matchesJsonSchemaInClasspath("user-list-schema.json"));
+    }
+
+    // userId 动态输入，年龄大于18，email需要符合@的邮箱格式
+    @Test
+    public void queryUserTest() {
+        Long userId = 1L;
+        requestSpecification
+                .when()
+                .get("/demo/swagger/user/simple/{userId}/info.htm", userId)
+                .then()
+                .assertThat()
+                .body("nickName", notNullValue())
+                .body("age", greaterThan(18))
+                .body("email", matchesRegex(".+@.+"));
+
     }
 
 
